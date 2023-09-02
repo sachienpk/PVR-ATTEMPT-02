029144da55e03f7b6908f044435796d34931276e
sachienpk <96417557+sachienpk@users.noreply.github.com>
2023-09-02 19:30:03 +0800

    Update record-website.js

--- record-website.js
+++ record-website.js
@@ -1,40 +1,31 @@
-async function recordWebsite() {
-  // Create a new MediaRecorder object.
-  const recorder = new MediaRecorder();
+const puppeteer = require("puppeteer");
 
-  // Get the URL of the website to record.
-  const url = "https://sastro-go.vercel.app/web-player/";
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-  // Open a headed browser to the website.
-  const browser = await navigator.permissions.query({ name: "microphone" });
-  if (browser.state === "granted") {
-    const win = await new Promise((resolve, reject) => {
-      const iframe = document.createElement("iframe");
-      iframe.src = url;
-      iframe.style.display = "none";
-      document.body.appendChild(iframe);
-      iframe.onload = () => resolve();
-    });
+  await page.goto("https://sastro-go.vercel.app/web-player/");
 
-    // Start recording the website.
-    recorder.start();
+  // Start recording the website.
+  const mediaStream = await page.mediaDevices.getDisplayMedia({
+    audio: false,
+  });
+  const recorder = new MediaRecorder(mediaStream);
 
-    // Wait for 30 seconds.
-    await new Promise((resolve, reject) => setTimeout(resolve, 30000));
+  // Wait for 30 seconds.
+  await new Promise((resolve, reject) => setTimeout(resolve, 30000));
 
-    // Stop recording the website.
-    recorder.stop();
+  // Stop recording the website.
+  recorder.stop();
 
-    // Save the recording as an MP4 file.
-    const blob = await recorder.getBlob();
-    const filename = `recording.mp4`;
-    const file = await navigator.clipboard.write(blob, { filename });
+  // Save the recording as an MP4 file.
+  const blob = await recorder.getBlob();
+  const filename = `recording.mp4`;
+  const file = await browser.saveFile(blob, filename);
 
-    // Notify the user that the recording has been saved.
-    alert(`The recording has been saved as ${filename}.`);
-  } else {
-    alert("The microphone permission is not granted.");
-  }
-}
+  // Notify the user that the recording has been saved.
+  console.log(`The recording has been saved as ${filename}.`);
 
-recordWebsite();
+  // Close the browser.
+  await browser.close();
+})();
